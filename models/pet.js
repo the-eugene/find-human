737af50d5b96d01cac6eb1141f9735909d12afc1
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2021-06-07 17:03:59 -0600

    Created Favorite, Pet, and User Schemas.

---
+++ models/pet.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const Pet = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  species: {
+    type: String,
+    required: true
+  },
+  breed: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  activityLevel: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  specialNeeds: { // not required
+    type: String,
+  }
+});
+
+
+module.exports = mongoose.model('Pet', Pet);
