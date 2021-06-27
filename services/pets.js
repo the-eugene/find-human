74fecc0689cb80ba14d5c7c4804e0223c8ca9955
hig19011 <hig19011@byui.edu>
2021-06-27 09:25:56 -0700

    Turned Pets Service into singleton

--- services/pets.js
+++ services/pets.js
@@ -4,9 +4,56 @@ const _=require('lodash');
 const key = process.env.DOG_API_KEY;
 const url = `${process.env.DOG_API_URL}`;
 
+class BreedsApi {
+  constructor() {
+    this.breeds = [];
+    this.breedTemperaments = [];
+    
+  }
 
-const getAllBreeds = async () =>
-  (await (
+  getAllBreeds = async () =>{
+    if(this.breeds.length > 0)  {      
+      return this.breeds;      
+    }
+   
+    this.breeds = (await (
+      await fetch(`${url}/breeds?attach_breed=0`, {
+        method: "GET",
+        headers: {
+          "x-api-key": key
+        },
+      })
+    ).json());
+
+    //console.log(this.breeds);
+    return this.breeds;
+  };
+
+  getDogBreeds = async () => (await this.getAllBreeds()).map(breed => breed.name);
+
+  // gets a list of strings for dog temperaments
+  getDogTemperaments = async () => {
+    if(this.breedTemperaments > 0) {
+      return this.breedTemperaments;
+    }
+    
+    let list = _.uniq((await this.getAllBreeds())
+      .map(breed => breed.temperament && breed.temperament.split(',')) // map from all info to just temperaments
+      .filter(temperament => !!temperament) // get rid of empty temperaments
+    ).sort();
+
+
+    list.forEach(x => this.breedTemperaments.push(...x));
+    this.breedTemperaments = _.uniq(this.breedTemperaments).sort();
+    return this.breedTemperaments;
+  }
+}
+
+exports.BreedsApi = new BreedsApi();
+
+const getAllBreeds = async () =>{
+  console.log("getting breeds old");
+  return (await (
     await fetch(`${url}/breeds?attach_breed=0`, {
       method: "GET",
       headers: {
@@ -14,7 +61,7 @@ const getAllBreeds = async () =>
       },
     })
   ).json());
-
+}
 // returns a list of all breed names
 exports.getDogBreeds = async () =>
   (await getAllBreeds()).map(breed => breed.name);
@@ -24,12 +71,7 @@ exports.getDogBreedGroups = async () =>
   _.uniq((await getAllBreeds()).map(breed => breed.breed_group)).sort()
     .filter(breed => !!breed);
 
-// gets a list of strings for dog temperaments
-exports.getDogTemperaments = async () =>
-  _.uniq((await getAllBreeds())
-    .map(breed => breed.temperament && breed.temperament.split(',')) // map from all info to just temperaments
-    .filter(temperament => !!temperament) // get rid of empty temperaments
-  ).sort();
+
 
 
 // fetch a dog by breed name
